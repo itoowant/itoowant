2b38154cf69d685bca1b6c79a3f4e5261aaa2f67
I Too <itoowant@gmail.com>
2011-02-16 01:42:33 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itoowant::Application.config.secret_token = '741c6a8b0b88196d3b57667bca7beaf63231199ca1fb542ea31d4f488917c35ddb0e4fe864df3babcba7acc5620612343a41871fb6a20e170eaa6637d3a6627c'
